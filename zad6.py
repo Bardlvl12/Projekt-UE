cf529b4baa5b3840c8c6f70c9afc432ed3f00be9
Bardlvl12 <149380192+Bardlvl12@users.noreply.github.com>
2023-12-18 10:59:11 +0100

    Update zad6.py

--- zad6.py
+++ zad6.py
@@ -5,23 +5,21 @@
 
 
 def polacz_i_przetworz(list1, list2):
-    # Połącz listy
+   
     polaczona_lista = list1 + list2
 
-    # Usuń duplikaty
     unikalna_lista = list(set(polaczona_lista))
 
-    # Podnieś każdy element do potęgi 3
     przetworzona_lista = [element ** 3 for element in unikalna_lista]
 
     return przetworzona_lista
 
-# Przykładowe użycie funkcji
+
 lista1 = [1, 2, 3, 4]
 lista2 = [3, 4, 5, 6]
 wynik = polacz_i_przetworz(lista1, lista2)
 
-# Wyświetlenie wyniku
+
 print(wynik)
 
 
